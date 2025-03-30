8e0bf80ec750e4c1ed8c61e8154b9b46d8521403
CydeWare <71936967+CydeWare@users.noreply.github.com>
2025-03-30 22:21:27 +0700

    Add files via upload

---
+++ server/db.js
@@ -0,0 +1,26 @@
+import mysql from "mysql2";
+
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT, // Railway might use a custom port
+    namedPlaceholders: true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000, // 10 seconds timeout
+}).promise();
+
+// Test the connection (optional)
+pool.getConnection()
+    .then((conn) => {
+        console.log("Connected to MySQL!");
+        conn.release();
+    })
+    .catch((err) => {
+        console.error("Error connecting to MySQL:", err);
+    });
+
+export default pool;
